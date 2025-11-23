193e26806de0a67fe9089f1de1747d7bc1945b21
Brajkishore-yadav <braj.2024ug2027@iiitranchi.ac.in>
2025-11-23 19:18:47 +0530

    Add function to find second largest element
    Implemented function to find the second largest element in a vector.

--- array/secondlargest.cpp
+++ array/secondlargest.cpp
@@ -1,7 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main (){
-        //largest k lga ke than condition slargest!=largest kr do
-        
+int secondLargest(vector<int>& v) {
+    int mx1 = INT_MIN;     // largest
+    int mx2 = INT_MIN;     // second largest
 
+    for(int x : v) {
+        if (x > mx1) {
+            mx2 = mx1;     // old largest becomes second largest
+            mx1 = x;       // new largest
+        }
+        else if (x > mx2 && x != mx1) {
+            mx2 = x;       // x is second largest
+        }
+    }
+
+    return mx2;
 }
